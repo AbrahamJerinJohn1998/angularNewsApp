d88877f61c5aabdaedb8a5696834f8fddc433f2b
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-29 08:40:57 +0530

    live news

---
+++ src/app/live-news/live-news.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-live-news',
+  templateUrl: './live-news.component.html',
+  styleUrls: ['./live-news.component.css']
+})
+export class LiveNewsComponent {
+
+}
